aec4450315be8910a78733659e5fa8ad90dda968
Daniel88dev <64728456+Daniel88dev@users.noreply.github.com>
2025-04-05 21:08:56 +0200

    Created initial client server

---
+++ client/src/server.ts
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+const PORT = 8091;
+
+app.get("/", (_, res) => {
+  res.send(`
+    <h2>gRPC Client is running</h2>
+    <ul>
+      <li><a href="/status">Status</a></li>
+      <li><a href="/users">Load users</a></li>
+      <li><a href="/login">Login test user</a></li>
+    </ul>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Client server running on http://localhost:${PORT}`);
+});
